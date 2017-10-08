1383b2562725788582093e39c777d7dc9379ce2f
mcrophonehead <alansimmons262@gmail.com>
2017-10-08 17:50:55 +0100

    I failed to use the menu control as a library, so I cut and paste it into this file.

--- flipping cards.py
+++ flipping cards.py
@@ -201,11 +201,13 @@ try:
 except:
     myerrors.append("Error initialising the pygame library.")
 
-try:
-    from menu import MnuCntrl
-except:
-    myerrors.append("Error importing library: menu.")
-
+##try:
+##    from menu import MnuCntrl
+##except:
+##    myerrors.append("Error importing library: menu.")
+#
+# I'm having real trouble using the MenuCtrl as a library, so I'v resorted to
+# coping and pasting it.
 
 FPS = 30 # frames per second setting
 #FPS = 3 # frames per second setting
@@ -243,6 +245,7 @@ except:
 BLACK =(0, 0, 0)
 BLUE =(0, 0, 200)
 GREEN =(0, 200, 0)
+GREY =( 50, 50, 50)
 RED =(200, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW =(200, 200, 0)
@@ -278,6 +281,296 @@ arr_card_place_holders = [
     
 
 score =0
+
+
+# --------------------------------------------------------------------
+# Description:
+#   The class has several routines to create, and manage a text based
+#   menu interface.
+#
+#   The common steps to creating a menu with this class is as follows:
+#       1.  Create a menu object.
+#       2.  Add menu items for the user to select.
+#       3.  Yield execution to menu control and wait for a menu item
+#           to be selected.
+#
+# Summary:
+#   Assists with the creation and management of a menu, including
+#   interpreting which menu item has been selected by the user.
+#
+# NOTE(S):
+#   The way I hacked the "type" keyword [str(type(txt_colour))] causes
+#   a problem by ensure its not country indepenndant.
+# --------------------------------------------------------------------
+class MnuCntrl():
+
+    # load the default fonts.
+##    print()
+##    print("Class MnuCntrl()")
+##    print("\tLoading menu fonts.")
+##    print()
+            
+    menu_def_list ={} # create a dictionary of default values to be used.        
+    mnu_item_cntrls = [] # prep list for menu items.
+    
+    # --------------------------------------------------------------------
+    def __init__(self):
+        # init def fonts to use with the menu.
+        
+        self.mnuhdr =MnuCntrl.menu_def_list["header font"] 
+        self.mnuitems_font =MnuCntrl.menu_def_list["items font"]
+        
+        self.items_col_active =MnuCntrl.menu_def_list["active colour"]
+        self.items_col_inactive =MnuCntrl.menu_def_list["inactive colour"]
+
+##        print()
+##        print("Class MnuCntrl()")
+##        print("\tMenu header font =",MnuCntrl.menu_def_list["header font"])
+##        print("\tMenu item font =",MnuCntrl.menu_def_list["items font"])
+##        print()
+    # __init__() ---------------------------------------------------------
+                
+    # --------------------------------------------------------------------
+    # Description:
+    #   This routine adds a new menu item for a menu object.
+    #
+    # Parameter(s):
+    #   x, y            Integers
+    #       Refer to the top left corner of the painted text.
+    #
+    #   item_caption    Str
+    #       The caption given to the menu item.
+    #
+    #   txt_colour      Tuple
+    #       Overrides the default colour of the menu item.
+    #       Note:   Routine expect the Tuple to be 3 dimensions, the normal
+    #               ranges of the RGB.
+    # --------------------------------------------------------------------
+    def add_item(self, x, y, item_caption, txt_colour =None):
+        self.img_surf =[]
+        looks_ok =False
+        
+        if txt_colour is None: # set the menu item to its default colour.
+            txt_colour =self.items_col_inactive
+        else:
+            # verify parameter is valid
+            #if "tuple" in str(type(txt_colour)):
+            if isinstance(txt_colour, tuple):
+                # variable is correct type.
+                if len(txt_colour) ==3:
+                    # things are looking good. Lets check if the
+                    # tuple is made up of integers.
+                    if isinstance(txt_colour[0], int):
+                        if isinstance(txt_colour[1], int):
+                            if isinstance(txt_colour[2], int):
+                                looks_ok =True
+                            else:
+                                # the third element was the wrong type.
+                                pass
+                        else:
+                            # the second element was the wrong type.
+                            pass
+                    else:
+                        # the first element was the wrong type.
+                        pass
+                    
+            if looks_ok ==False:
+                # we got a problem, the calling routine has passed
+                # the wrong variable type.
+
+                # resort to using the default colour.
+                txt_colour =self.items_col_inactive
+##                print("Resorted to using the default menu colour for a menu item control")
+
+                           
+        # lets add image refs ...
+        #   - the colour of menu item text not clicked.
+        #   - the colour of the bevel for the menu item.
+        mf =self.mnuitems_font
+
+        try:
+            self.img_surf.append(mf.render(item_caption, True, txt_colour))
+        except TypeError:
+            # resort to using the default colour.
+            txt_colour =self.items_col_inactive
+            
+            # make a final attemt at rendering the text using the
+            # new configuration.
+            self.img_surf.append(mf.render(item_caption, True, txt_colour))
+##            print("Resorted to using the default menu colour for a menu item control")
+            
+        self.img_surf.append(mf.render(item_caption, True, self.items_col_active))
+        self.img_surf.append(mf.render(item_caption, True, GREY))
+        self.img_rect =self.img_surf[0].get_rect()
+        
+        w =self.img_rect.width
+        h = self.img_rect.height
+        self.mnu_item_cntrls.append([x, y, w, h, self.img_surf, item_caption])
+        return self.img_surf
+    # add_item() -------------------------------------------------------
+    
+    # --------------------------------------------------------------------
+    # Description:
+    #   This routine paints all the menu items on the screen for the
+    #   user to see.
+    # --------------------------------------------------------------------
+    def paint(self):
+        # examine all the menu items controls and
+        # paint them on the screen.
+        for mnuitem in self.mnu_item_cntrls:
+            # get the co-ordinates for the menu item.
+            x =mnuitem[0]
+            y =mnuitem[1]
+            # start by painting the bevel for the text.
+            for n in range(5):
+                img_rect =mnuitem[4][0].get_rect()
+                img_rect.topleft = (x+n, y+n)
+                DISPLAYSURF.blit(mnuitem[4][2], img_rect)
+
+            # paint the primary text on top to give bevel effect.
+            img_rect =mnuitem[4][0].get_rect()
+            img_rect.topleft = (x, y)
+            DISPLAYSURF.blit(mnuitem[4][0], img_rect)
+            pygame.display.flip()
+        return True
+    # paint() ------------------------------------------------------------
+    
+    # --------------------------------------------------------------------
+    # Description:
+    #   This routine examines the (passed) mouse co-ordinates and alters
+    #   the menu items status to active and inactivet acordingly based on
+    #   the players specified position.
+    # --------------------------------------------------------------------
+    def paint_items_state(self, mousexy):
+        mnu_option =0
+        msex, msey =mousexy
+        for mnuitem in self.mnu_item_cntrls:
+            mnu_option +=1
+            w =mnuitem[2]
+            h =mnuitem[3]
+            active_button =False
+            
+            # get the co-ordinates for the menu item controls.
+            x =mnuitem[0]
+            y =mnuitem[1]
+            # _.
+            
+            if msex >= x and msex <=x +w:
+                if msey >= y and msey <=y +h:
+                    # this button was clicked.
+                    active_button =True
+            if active_button ==True:
+                # paint the menu item as active.
+                img_rect =mnuitem[4][1].get_rect()
+                img_rect.topleft = (x, y)
+                DISPLAYSURF.blit(mnuitem[4][1], img_rect)
+            else:
+                # paint the menu item as inactive.
+                img_rect =mnuitem[4][0].get_rect()
+                img_rect.topleft = (x, y)
+                DISPLAYSURF.blit(mnuitem[4][0], img_rect)
+                
+        pygame.display.flip()
+    # paint_items_state() ----------------------------------------------
+            
+    # --------------------------------------------------------------------
+    # Description:
+    #   This routine removes all the menu items, and frees the
+    #   image buffer
+    #
+    # Issues:
+    #   I don't think so!!!
+    # --------------------------------------------------------------------
+    def reset(self):
+        # need to figure out how to free the image buffer >???<
+        
+        mnu_item_cntrls =[]
+    # reset() ------------------------------------------------------------
+    
+    # --------------------------------------------------------------------
+    # Description:
+    #   This routine waits for the user to select a menu item with the
+    #   mouse.
+    # --------------------------------------------------------------------
+    def wait_ui(self):
+        events =pygame.event.get() # clear kbd and mouse buffer.
+    
+        ui_ok =False
+        while ui_ok ==False:
+            #check for player input.
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                elif event.type == MOUSEBUTTONUP:
+                    # Handle mouse click events
+                    mousex, mousey = event.pos
+                    
+                    mnu_option =0
+                    for cmdbutton in self.mnu_item_cntrls:
+                        mnu_option +=1
+                        w =cmdbutton[2]
+                        h =cmdbutton[3]
+                        if mousex >= cmdbutton[0] and mousex <=cmdbutton[0] +w:
+                            if mousey >= cmdbutton[1] and mousey <=cmdbutton[1] +h:
+                                # this button was clicked.
+##                                print("Menu item {} clicked.".format(mnu_option))
+                                ui_ok =True
+                                break
+                elif event.type ==4:
+                    # then its a mouse move event (?)
+                    self.paint_items_state(event.pos)
+                else:
+                    # This is an event type that I haven't catered for.
+                    pass
+                
+            pygame.display.flip()
+            
+        return cmdbutton
+    # wait_ui() -------------------------------------------------------
+
+    # --------------------------------------------------------------------
+    # Description:
+    #   Loads a specified font and returns the active font obj ref if
+    #   possible.
+    #
+    #   The purpose of this wrapper routine is to trap any errors that
+    #   might be encountered when selecting a font and resolve issues of
+    #   incompatible fonts on different platforms by resorting to the
+    #   default font when problems occur.
+    # --------------------------------------------------------------------
+    def wr_load_font(fnt_name, fnt_size):
+        try:
+            # try and load the desired font.
+            cf =pygame.font.Font(fnt_name, fnt_size)
+        except:
+            try:
+                # resort to loading the default font instead.
+                cf =pygame.font.Font(None, fnt_size)
+            except:
+                cf =None
+##        print()
+##        print("menu.py - Class MnuCntrl().wr_load_font - Font =", cf)
+##        print()
+        
+        return cf # ref to currently (loaded) font.
+    # wr_load_font() --------------------------------------------------------
+
+    # using a dictionary object to make the variable data persistant (???)
+    menu_def_list["items font"] =wr_load_font('freesansbold.ttf', 60)
+    menu_def_list["header font"] =wr_load_font('freesansbold.ttf', 30)
+##    menu_def_list["items font"] =pygame.font.Font('freesansbold.ttf', 60)
+##    menu_def_list["header font"] =pygame.font.Font('freesansbold.ttf', 30)
+    
+    menu_def_list["active colour"] =YELLOW
+    menu_def_list["inactive colour"] =WHITE
+    
+# Class MnuCntrl() --------------------------------------------------------
+ 
 
 
 # define a structure type to hold information on cards
@@ -402,8 +695,9 @@ cardimages = []
 #   This routine clears the screen.
 # ---------------------------------------------------------------------------
 def cls():
-     # clear the screen.
-    pygame.draw.rect(DISPLAYSURF, BLACK, (0, 0, WINWIDTH, WINHEIGHT))
+    # clear the screen.
+    #pygame.draw.rect(DISPLAYSURF, BLACK, (0, 0, WINWIDTH, WINHEIGHT))
+    DISPLAYSURF.fill(BLACK)
 # cls() ---------------------------------------------------------------------
 
 
@@ -2304,36 +2598,6 @@ tmr_str =""
 
 game_init() # initialise the game.
 
-##exitgame =False
-##while exitgame ==False:
-##    #play_title_screen()
-##    
-##    game_begin()
-##    play_title_screen()
-##
-##    #print( playername_get() )
-##    
-##    ls =True # trigger flag so we can enter loop.
-##    while ls ==True:
-##        
-##        tsil = (glevel * 2)     # calc total stages in level.
-##        
-##        level_ini()
-##        level_begin()
-##        ls =level_play()
-##        level_end()
-##
-##        glevel +=1
-##
-##    #score +=1000   # add cheat so we can get on the high score table.
-##    
-##    if score >hstable_min_get(): # we have a new high score
-##        new_name =playername_get()
-##        hstable_insert(new_name, score)
-##        hstable_save()  # save the hs table.
-##        
-##    game_end()
-    
 exitgame =False
 while exitgame ==False:
     game_begin()
@@ -2359,64 +2623,23 @@ while exitgame ==False:
             new_name =playername_get()
             hstable_insert(new_name, score)
             hstable_save()  # save the hs table.
+        game_end()
             
     elif menu_item =="Hi Scores Table":
+        cls()
         hstable_paint()
     #elif menu_item =="Help":
     #    pass
     elif menu_item =="Quit":
-        game_end()
+        pygame.quit()
+        sys.exit()
         
     
         
-    game_end()
+    
 
     
 print("Player scored ", score)
 # ---------------------------------------------------------------------------
 
    
-
-
-
-### ---------------------------------------------------------------------------
-### move the card around the screen
-##while True: # the main game loop
-##    DISPLAYSURF.fill(BLACK)
-##    
-##    if direction == 'right':
-##        catx += 5
-##        if catx == 280:
-##            direction = 'down'
-##    elif direction == 'down':
-##        caty += 5
-##        if caty == 220:
-##            direction = 'left'
-##    elif direction == 'left':
-##        catx -= 5
-##        if catx == 10:
-##            direction = 'up'
-##    elif direction == 'up':
-##        caty -= 5
-##        if caty == 10:
-##            direction = 'right'
-##            
-##
-##    
-##    # paint a card image to the graphics buffer.
-##    DISPLAYSURF.blit(cardimages[ 3 ], (catx, caty))
-##    
-##    # scan input devices to check for user input.
-##    for event in pygame.event.get():
-##        if event.type == QUIT:
-##            # a close request on the window was made.
-##            pygame.quit()
-##            sys.exit()
-##        elif event.type == KEYDOWN:
-##            if event.key == K_ESCAPE:
-##                pygame.quit()
-##                sys.exit()
-##            
-##    #pygame.display.update()
-##    pygame.display.flip()
-##    fpsClock.tick(FPS)
